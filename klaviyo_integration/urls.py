5935710ef3f785ac278a88b116edff5e9fbe7e02
AzureAD\ShpendPalushi <shpalushi@ritech.co>
2021-07-19 10:58:13 +0200

    Added t

--- klaviyo_integration/urls.py
+++ klaviyo_integration/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 import requests;
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api_data.urls'))
+    path('', include('api_data.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]

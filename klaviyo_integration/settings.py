e05482cd24ed47ed7c0be1514e8b129c3db78b0e
AzureAD\ShpendPalushi <shpalushi@ritech.co>
2021-07-19 15:55:13 +0200

    Cors

--- klaviyo_integration/settings.py
+++ klaviyo_integration/settings.py
@@ -36,17 +36,30 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api_data',
     'rest_framework',
+    'corsheaders',
     'gunicorn'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 ROOT_URLCONF = 'klaviyo_integration.urls'

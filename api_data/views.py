a5e4f526b77ac93873f19829b676bf2ab7893a36
AzureAD\ShpendPalushi <shpalushi@ritech.co>
2021-07-21 15:17:11 +0200

    Added sync data

--- api_data/views.py
+++ api_data/views.py
@@ -15,6 +15,53 @@ import dotenv
 from klaviyo_integration.settings import BASE_DIR
 
 
+def get_course_data(course_sku):
+    if course_sku == "course-v1:OXF+FIN+03-2021":
+        return ["Register", "Oxford Fintech", "31 March 2021"]
+    elif course_sku == "course-v1:OXF+FIN+06-2021":
+        return ["Register", "Oxford Fintech", "9 June 2021"]
+    elif course_sku == "course-v1:OXF+FIN+10-2021":
+        return ["Register", "Oxford Fintech", "13 Oct 2021"]
+    elif course_sku == "course-v1:OXF+FIN+01-2022":
+        return ["Register", "Oxford Fintech", "19 January 2022"]
+    elif course_sku == "course-v1:OXF+CYB+03-2021":
+        return ["Register", "Oxford Cyber Security", "31 March 2021"]
+    elif course_sku == "course-v1:OXF+CYB+06-2021":
+        return ["Register", "Oxford Cyber Security", "16 June 2021"]
+    elif course_sku == "course-v1:OXF+CYB+10-2021":
+        return ["Register", "Oxford Cyber Security", "13 Oct 2021"]
+    elif course_sku == "course-v1:OXF+CYB+01-2022":
+        return ["Register", "Oxford Cyber Security", "12 January 2022"]
+    elif course_sku == "course-v1:OXF+AIF+03-2021":
+        return ["Register", "Oxford AI in Finance", "26 May 2021"]
+    elif course_sku == "course-v1:OXF+AIF+10-2021":
+        return ["Register", "Oxford AI in Finance", "13 Oct 2021"]
+    elif course_sku == "course-v1:OXF+AIF+01-2022":
+        return ["Register", "Oxford AI in Finance", "19 January 2022"]
+    elif course_sku == "course-v1:MIT+HVN+06-2021":
+        return ["Register", "MIT Leading Health Tech Innovation", "16 June 2021"]
+    elif course_sku == "course-v1:MIT+HVN+10-2021":
+        return ["Register", "MIT Leading Health Tech Innovation", "29 September 2021"]
+    elif course_sku == "course-v1:MIT+AIL+07-2021":
+        return ["Register", "MIT AI Leadership", "7 July 2021"]
+    elif course_sku == "course-v1:MIT+AIL+10-2021":
+        return ["Register", "MIT AI Leadership", "6 October 2021"]
+    elif course_sku == "TEST+TST101+2021_T1":
+        return ["Register", "Test Product", "22 September 2021"]
+    elif course_sku == "course-v1:OXF+BCH+09-2021":
+        return ["Register", "Oxford Blockchain Strategy", "22 September 2021"]
+    elif course_sku == "course-v1:OXF+PLT+10-2021":
+        return ["Register", "Oxford Platforms and Digital Disruption Programme", "6 October 2021"]
+    elif course_sku == "course-v1:OXF+PLT+01-2022":
+        return ["Register", "Oxford Platforms and Digital Disruption Programme", "19 January 2022"]
+    elif course_sku == "course-v1:CAM+STF+10-2021":
+        return ["Register", "Cambridge Startup Funding Pre-seed to Exit Programme", "13 October 2021"]
+    elif course_sku == "course-v1:CAM+REG+10-2021":
+        return ["Register", "Cambridge RegTech", "20 October 2021"]
+    elif course_sku == "course-v1:MIT+AIS+10-2021":
+        return ["Register", "MIT AI + Data Strategy", "20 October 2021"]
+
+
 @method_decorator(csrf_exempt, name="dispatch")
 class KlaviyoData(View):
 
@@ -37,14 +84,10 @@ class KlaviyoData(View):
         work_experience = data.get("work_experience")
         pay_type = data.get("pay_type")
         course_sku = data.get("course_name")
-        course_name = ""
-        lead_type = ""
-        course_start_date = '01-01-2020'
+        course_name = get_course_data(course_sku)[1]
+        lead_type = get_course_data(course_sku)[0]
+        course_start_date = get_course_data(course_sku)[2]
 
-        if course_sku == "course-v1:MIT+AIL+10-2021":
-            course_name = "MIT AI Leadership"
-            lead_type = "Register"
-            course_start_date = '01-07-2020'
         # end: get the user data from request
 
         # get api key from secrets file
@@ -87,4 +130,6 @@ class KlaviyoData(View):
             return JsonResponse({"success": True}, status=200)
         else:
             return JsonResponse({"success": False}, status=400)
+
+
 
